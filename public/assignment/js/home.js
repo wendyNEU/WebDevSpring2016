59a7df3ef8aec56a236c07e22702b66a7396da0b
wendyNEU <qiudilucky@gmail.com>
2016-01-26 18:57:53 -0500

    write main frame and add subpage jump javascript

---
+++ public/assignment/js/home.js
@@ -0,0 +1,48 @@
+/**
+ * Created by wendy on 1/26/16.
+ */
+
+
+$( document ).ready( function() {
+
+    $("#nav_register").click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $("#subpage").load("./register.html");
+    });
+    $("#nav_login").click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $("#subpage").load("./login.html");
+    });
+    $("#nav_profile").click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $("#subpage").load("./profile.html");
+    });
+    $("#nav_logout").click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $("#subpage").load("./logout.html");
+    });
+    $("#navside_home").click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $("#subpage").load("./home.html");
+    });
+    $("#navside_profile").click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $("#subpage").load("./profile.html");
+    });
+    $("#navside_admin").click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $("#subpage").load("./admin.html");
+    });
+    $("#navside_forms").click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $("#subpage").load("./forms.html");
+    });
+});
