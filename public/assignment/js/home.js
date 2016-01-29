99e21484b54e128ffcc3e0f95074cfb87188600b
wendyNEU <qiudilucky@gmail.com>
2016-01-28 22:42:17 -0500

    modify

--- public/assignment/js/home.js
+++ public/assignment/js/home.js
@@ -4,6 +4,7 @@
 
 
 $( document ).ready( function() {
+    var newWindow;
 
     $("#nav_register").click(function(e){
         e.preventDefault();
@@ -50,13 +51,83 @@ $( document ).ready( function() {
         e.stopPropagation();
         $("#subpage").load("./forms.html");
     });
-    $("#tagfields").click(function(e){
-        e.preventDefault();
-        e.stopPropagation();
-        $("#subpage").load("./form-fields.html");
-    });
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
         var target = $(e.target).attr("href") // activated tab
         alert(target);
+    });
+
+    $("#new_field").click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        console.log("new field");
+        /*
+         var label = $('#label').val();//document.getElementById("label").nodeValue;
+         var placeholder = $('#placeholder').val();//document.getElementById("placeholder").nodeValue;
+         var tooltip = $('#tooltip').val();//document.getElementById("tooltip").nodeValue;
+
+
+         var type = $( "#type option:selected" ).text();
+         var options = $("textarea#options").val().split("\n");
+         var existfields = document.getElementById("exist_fields");
+
+         var section = document.createElement("div");
+         section.setAttribute("class","form-group row");
+
+         var buttonpencil = document.createElement("button");
+         buttonpencil.setAttribute("class","btn btn-primary glyphicon glyphicon-pencil");
+         var buttonplus = document.createElement("button");
+         buttonplus.setAttribute("class","btn btn-primary glyphicon glyphicon-plus");
+         var buttonremove = document.createElement("button");
+         buttonremove.setAttribute("class","btn btn-primary glyphicon glyphicon-remove");
+         var buttonalignjustify = document.createElement("button");
+         buttonalignjustify.setAttribute("class","btn btn-primary glyphicon glyphicon-align-justify");
+         var buttongroup = document.createElement("div");
+         buttongroup.setAttribute("class","col-sm-6 col-md-6");
+         buttongroup.appendChild(buttonpencil);
+         buttongroup.appendChild(buttonplus);
+         buttongroup.appendChild(buttonremove);
+         buttongroup.appendChild(buttonalignjustify);
+
+         var field = document.createElement("div");
+         field.setAttribute("class","col-sm-6 col-md-6");
+         var labele = document.createElement("label");
+         labele.setAttribute("class","form-control-label");
+         labele.setAttribute("for",""+label);
+         labele.createTextNode(""+label);
+         field.appendChild(label);
+
+
+         if(type==='Single Line Text Field'){
+         var input = document.createElement("input");
+         input.setAttribute("type","text");
+         input.setAttribute("class","form-control");
+         input.setAttribute("id",""+label);
+         input.setAttribute("placeholder",placeholder+"");
+
+         field.appendChild(input);
+
+         section.appendChild(field);
+         section.appendChild(buttongroup);
+
+         }else if(type==='Date Field'){
+
+         }else if(type==='Dropdown Field'){
+
+         }else if(type==='Checkboxes Field'){
+
+         }else if(type==='Radio Buttons Field'){
+
+         }
+         existfields.appendChild(section);
+         */
+         newWindow.close();
+    });
+    $("#add_fields").click(function (e) {
+        newWindow = window.open('./form-fields.html', "Fields Setting", "width=350,height=250");
+
+        // Puts focus on the newWindow
+        if (window.focus) {
+            newWindow.focus();
+        }
     });
 });
