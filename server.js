4e73d22de50a6f5d525406cd78d585f77338e6a4
wendyNEU <qiudilucky@gmail.com>
2016-04-11 16:32:06 -0400

    add security server

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 var express       = require('express');
 var app           = express();
 var bodyParser    = require('body-parser');
-/*
 var multer        = require('multer');
 var passport      = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 var cookieParser  = require('cookie-parser');
 var session       = require('express-session');
-*/
 var mongoose      = require('mongoose');
 
 var connectionString = 'mongodb://127.0.0.1:27017/webdev2016';
@@ -25,13 +22,15 @@ var db = mongoose.connect(connectionString);
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-/*
-app.use(multer());
-app.use(session({ secret: process.env.PASSPORT_SECRET,resave: true, saveUninitialized: true}));
-app.use(cookieParser())
+multer();
+app.use(session({
+    secret: 'this is the secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
-*/
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
